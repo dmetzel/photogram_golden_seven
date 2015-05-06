03ed8e77602bee6dde3d4381acad868803c2381d
dmetzel <dmetzel2016@kellogg.northwestern.edu>
2015-05-06 00:25:49 -0500

    Finished HW

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,69 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+
+p = Photo.find(params[:id])
+
+@source = p.source
+@caption = p.caption
+
+end
+
+
+def new_form
+
+end
+
+def create_row
+
+p = Photo.new
+
+p.source = params[:the_source]
+p.caption = params[:the_caption]
+
+p.save
+
+redirect_to("http://localhost:3000/photos")
+
+end
+
+def destroy
+
+p = Photo.find(params[:id])
+
+p.destroy
+
+redirect_to("http://localhost:3000/photos")
+
+end
+
+def edit_form
+
+@id = params[:id]
+
+p = Photo.find(@id)
+
+@source = p.source
+@caption = p.caption
+
+end
+
+def update_row
+
+p = Photo.find(params[:id])
+
+p.source = params[:the_source]
+p.caption = params[:the_caption]
+
+p.save
+
+redirect_to("http://localhost:3000/photos")
+
+end
+
+def new_form
+
+end
 
 end
